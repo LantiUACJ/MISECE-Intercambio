b71b4b1871514dae90cdad091c2d3e2b4206ca17
marcom333 <dev.marco.medina@gmail.com>
2022-04-18 13:58:30 -0600

    revertir interfaz de usuario

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -11,12 +11,14 @@
         <style>
             .bloque{
                 margin-bottom: 10px;
+                /*background-color: rgba(0,0,0,0.2);*/
             }
             div{
                 word-wrap: break-word;
             }
             div.row{
                 border: 1px solid black;
+                width: 100%;
             }
             table{
                 border: 1px solid black;
@@ -24,18 +26,113 @@
             td{
                 border: 1px solid black;
             }
+
+            .patient{
+                /*background-color: rgb(255, 236, 236) !important;*/
+            }
+            .encounter{
+                /*background-color: rgb(202, 202, 202) !important;*/
+            }
+            .observation{
+                /*background-color: rgb(209, 247, 165) !important;*/
+            }
+            .medicationAdmin{
+                /*background-color: #68d4d8 !important;*/
+            }
+            .diagnosticReport{
+                /*background-color: #abddab !important;*/
+            }
+            .imagingStudy{
+                /*background-color: #caaef7 !important;*/
+            }
+            .medication{
+                /*background-color: #dbee87 !important;*/
+            }
+            .organization{
+                /*background-color: #57aeff !important;*/
+            }
+            .practitioner{
+                /*background-color: #80a389 !important;*/
+            }
+            .procedure{
+                /*background-color: #d3bfda !important;*/
+            }
         </style>
         <title>Document</title>
     </head>
     <body>
         <div class="container">
-            @foreach ($data as $expediente)
-                @foreach ($expediente["bundle"]->entry as $entry)
-                    @if ($entry->resource->resourceType == "Patient")
-                        <h2>Paciente</h2>
-                        @include('_fhir.resource.patient', ["obj"=>$entry->resource])
+            @if (!$data)
+                <h1>No hay resultados...</h1>
+            @endif
+            @foreach ($data as $bundle)
+                @if ($bundle["bundle"] && gettype($bundle["bundle"])!="array")
+                    <h1>Hospital: {{$bundle["hospital"]->user}}</h1>
+                    @foreach ($bundle["bundle"]->entry as $entry)
+                        @if (isset($entry->resource->resourceType) && $entry->resource->resourceType == "Patient")
+                            <div class="patient">
+                                @include('fhir.resource.patient',["obj"=>$entry->resource])
+                            </div><br><br>
+                        @endif
+                        @if (isset($entry->resource->resourceType) && $entry->resource->resourceType == "Observation")
+                            @if ( isset($entry->resource->encounter))
+                                <div class="observation">
+                                    @include('fhir.resource.observation',["obj"=>$entry->resource])
+                                </div><br><br>
+                            @endif
+                        @endif
+                        @if (isset($entry->resource->resourceType) && $entry->resource->resourceType == "MedicationAdministration")
+                            <div class="medicationAdmin">
+                                @include('fhir.resource.medicationAdministration',["obj"=>$entry->resource])
+                            </div><br><br>
+                        @endif
+                        @if (isset($entry->resource->resourceType) && $entry->resource->resourceType == "Encounter")
+                            <div class="encounter">
+                                @include('fhir.resource.encounter',["obj"=>$entry->resource, "fullUrl"=>$entry->fullUrl])
+                            </div><br><br>
+                        @endif
+                        @if (isset($entry->resource->resourceType) && $entry->resource->resourceType == "DiagnosticReport")
+                            <div class="diagnosticReport">
+                                @include('fhir.resource.diagnosticReport',["obj"=>$entry->resource])
+                            </div><br><br>
+                        @endif
+                        @if (isset($entry->resource->resourceType) && $entry->resource->resourceType == "ImagingStudy")
+                            <div class="imagingStudy">
+                                @include('fhir.resource.imagingStudy',["obj"=>$entry->resource])
+                            </div><br><br>
+                        @endif
+                        @if (isset($entry->resource->resourceType) && $entry->resource->resourceType == "Medication")
+                            <div class="medication">
+                                @include('fhir.resource.medication',["obj"=>$entry->resource])
+                            </div><br><br>
+                        @endif
+                        @if (isset($entry->resource->resourceType) && $entry->resource->resourceType == "Organization")
+                            <div class="organization">
+                                @include('fhir.resource.organization',["obj"=>$entry->resource])
+                            </div><br><br>
+                        @endif
+                        @if (isset($entry->resource->resourceType) && $entry->resource->resourceType == "Practitioner")
+                            <div class="practitioner">
+                                @include('fhir.resource.practitioner',["obj"=>$entry->resource])
+                            </div><br><br>
+                        @endif
+                        @if (isset($entry->resource->resourceType) && $entry->resource->resourceType == "Procedure")
+                            <div class="procedure">
+                                @include('fhir.resource.procedure',["obj"=>$entry->resource])
+                            </div><br><br>
+                        @endif
+                        @if (!isset($entry->resource->resourceType))
+                            {{dd($entry)}}
+                        @endif
+                    @endforeach
+                @else
+                    @if (gettype($bundle)=="array"&&isset($bundle["error"]))
+                        <h1>{{$bundle["error"]}}</h1>
+                    @else
+                        <h1>No hay resultados...</h1>    
                     @endif
-                @endforeach
+                    
+                @endif
             @endforeach
         </div>
         <!-- Optional JavaScript -->
