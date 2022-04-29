2e98575cc99262285f183d4fda588c44b58c6fe2
Syscore1 <32805386+Syscore1@users.noreply.github.com>
2022-04-29 11:57:11 -0600

    add aws files

---
+++ tests/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /
+cd var/www/html/MISECE-Intercambio
+php artisan migrate --force
+#php artisan config:clear
+#php artisan route:clear
+#php artisan config:cache
+#php artisan route:cache
+#php artisan optimize
+sudo -u ec2-user composer install
+sudo -u ec2-user composer dumpautoload -o
+sudo yum update -y
