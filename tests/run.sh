e6b08725158fbd236454a64df093b04c638da6bf
Syscore1 <32805386+Syscore1@users.noreply.github.com>
2022-05-24 12:22:31 -0700

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-#sudo yum update -y
 cd /
 cd var/www/html/MISECE-Intercambio
 sudo rm .env
 wget https://s3intercambio2.s3.amazonaws.com/.env
 php artisan migrate --force
 php artisan config:clear
-#php artisan route:clear
-#php artisan config:cache
-#php artisan route:cache
-#php artisan optimize
-#sudo -u ec2-user composer install
-#sudo -u ec2-user composer dumpautoload -o
+sudo -u ec2-user composer install
+
