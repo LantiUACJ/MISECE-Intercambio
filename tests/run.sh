9a4941c066bed37a4ead4edd7a382572c34fa927
Syscore1 <32805386+Syscore1@users.noreply.github.com>
2022-07-05 14:15:08 -0600

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -2,4 +2,4 @@
 cd /
 cd var/www/html/MISECE-Intercambio
 #sudo rm .env
-#aws s3 cp s3://s3intercambio2/.env /var/www/html/MISECE-Intercambio/
+sudo aws s3 cp s3://s3intercambio2/.env /var/www/html/MISECE-Intercambio/
