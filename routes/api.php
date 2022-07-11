19bd8318144a1b2459444e877059557530921dd7
marcom333 <dev.marco.medina@gmail.com>
2022-07-11 10:36:34 -0600

    agregado modo prueba

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\ApiController as ApiControllerV1;
+use App\Http\Controllers\V1\TestApiController as TestApiControllerV1;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +15,11 @@ use App\Http\Controllers\V1\ApiController as ApiControllerV1;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::prefix('/v1')->name('admin.')->middleware(["auth.api"])->group(function(){
+Route::prefix('/v1')->middleware(["auth.api"])->group(function(){
     Route::post('/expediente/{curp}', [ApiControllerV1::class, "consultarExpedientes"]);
-});
-
-Route::prefix('/v1')->name('admin.')->middleware(["auth.api"])->group(function(){
     Route::post('/expediente/basico/{curp}', [ApiControllerV1::class, "consultarExpedientesBasico"]);
+    /* Testing routes */
+    Route::post('/test/expediente/{curp}', [TestApiControllerV1::class, "consultarExpedientes"]);
+    Route::post('/test/expediente/basico/{curp}', [TestApiControllerV1::class, "consultarExpedientesBasico"]);
+    Route::post('/test/json', [TestApiControllerV1::class, "testJson"]);
 });
