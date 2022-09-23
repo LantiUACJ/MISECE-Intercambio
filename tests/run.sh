6e1d67e0e665920b5d491fa07f7dc83a59815992
Syscore1 <32805386+Syscore1@users.noreply.github.com>
2022-09-23 13:31:23 -0600

    Composer y migrate

--- tests/run.sh
+++ tests/run.sh
@@ -5,3 +5,5 @@ cd var/www/html/MISECE-Intercambio
 sudo rm .env
 echo removed 
 sudo aws s3 cp s3://s3intercambio2/.env /var/www/html/MISECE-Intercambio/ > /dev/null 2> /dev/null < /dev/null &
+composer update > /dev/null 2> /dev/null < /dev/null &
+php artisan migrate > /dev/null 2> /dev/null < /dev/null &
