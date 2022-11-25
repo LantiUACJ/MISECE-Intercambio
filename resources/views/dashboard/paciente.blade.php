aa9774c2fae8bce4176b78ae565fbd857ebcb1a6
Syscore1 <32805386+Syscore1@users.noreply.github.com>
2022-11-25 11:20:10 -0600

    Eliminacion texto no necesario

--- resources/views/dashboard/paciente.blade.php
+++ resources/views/dashboard/paciente.blade.php
@@ -5,8 +5,8 @@
         <div class="card padding">
             <div class="row">
                 <div class="col s12">
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea corrupti aperiam tenetur ipsum doloribus nostrum. Ipsa culpa pariatur sint fugit itaque sequi tempora perspiciatis, voluptatum, earum optio a aperiam expedita.</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut, maxime. Voluptatum unde in architecto laborum autem sequi asperiores, neque minima veritatis, nesciunt soluta est accusantium fugit voluptatibus recusandae nostrum quae.</p>
+                    <p></p>
+                    <p></p>
                 </div>
             </div>
         </div>
